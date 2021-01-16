f63a35dfbbb778c3580868c6ac3ede893339cd4d
Wingtail <sr9074@gmail.com>
2021-01-17 06:41:31 +0900

    completed email module

--- email_module.py
+++ email_module.py
@@ -2,7 +2,7 @@ import smtplib
 import time
 import imaplib
 import email
-import traceback 
+import traceback
 import configparser
 # -------------------------------------------------
 #
@@ -23,40 +23,46 @@ class Gmail:
         s, encoding = info[0]
         return s, encoding
 
-    def read_email(self, email_id, whitelist):
+    def _read_email(self, mail, email_id, whitelist):
         try:
-            data = mail.fetch(str(latest_email_id), '(RFC822)' )
+            data = mail.fetch(str(email_id), '(RFC822)')
             for response_part in data:
                 arr = response_part[0]
                 if isinstance(arr, tuple):
                     email_message = email.message_from_string(str(arr[1],'utf-8'))
-                    print('From: ', email_message['From'])
-                    print('Sender: ', email_message['Sender'])
-                    print('Date: ', email_message['Date'])
+                    sender = email_message['From']
+                    if any(address in sender for address in whitelist):
+                        email_dict = {}
+                        email_dict['sender'] = email_message['From']
+                        email_dict['date'] = email_message['Date']
 
-                    subject, encode = self.find_encoding_info(email_message['Subject'])
+                        subject, encode = self.find_encoding_info(email_message['Subject'])
 
-                    message = ''
+                        message = ''
 
-                    print('[Message]')
-                    if email_message.is_multipart():
-                        for part in email_message.get_payload():
-                            if part.get_content_type() == 'text/plain':
-                                bytes = part.get_payload(decode=True)
-                                encode = part.get_content_charset()
-                                message = message + str(bytes, encode)
+                        if email_message.is_multipart():
+                            for part in email_message.get_payload():
+                                if part.get_content_type() == 'text/plain':
+                                    bytes = part.get_payload(decode=True)
+                                    encode = part.get_content_charset()
+                                    message = message + str(bytes, encode)
+                        else:
+                            if email_message.get_content_type() == 'text/plain':
+                                bytes = email_message.get_payload(decode=True)
+                                encode = email_message.get_content_charset()
+                                message = str(bytes, encode)
+                        email_dict['msg'] = message
+
+                        return email_dict
                     else:
-                        if email_message.get_content_type() == 'text/plain':
-                            bytes = email_message.get_payload(decode=True)
-                            encode = email_message.get_content_charset()
-                            message = str(bytes, encode)
-                    print(message)
+                        return None
+
         except Exception as e:
             print("Fatal Error")
-            traceback.print_exc() 
+            traceback.print_exc()
             print(str(e))
 
-    def read_latest_email(self):
+    def read_latest_emails(self, whitelist, n_emails_before=100):
         try:
             mail = imaplib.IMAP4_SSL(self.SMTP_SERVER)
             mail.login(self.FROM_EMAIL, self.FROM_PWD)
@@ -64,72 +70,29 @@ class Gmail:
 
             data = mail.search(None, 'ALL')
             mail_ids = data[1]
-            id_list = mail_ids[0].split()[-20:]
+            id_list = mail_ids[0].split()[-n_emails_before:]
 
             first_email_id = int(id_list[0])
             latest_email_id = int(id_list[-1])
-            data = mail.fetch(str(latest_email_id), '(RFC822)' )
-            for response_part in data:
-                arr = response_part[0]
-                if isinstance(arr, tuple):
-                    email_message = email.message_from_string(str(arr[1],'utf-8'))
-                    print('From: ', email_message['From'])
-                    print('Sender: ', email_message['Sender'])
-                    print('Date: ', email_message['Date'])
 
-                    subject, encode = self.find_encoding_info(email_message['Subject'])
+            whitelist_emails = []
 
-                    message = ''
+            for i in range(latest_email_id, first_email_id, -1):
+                mail_content = self._read_email(mail, i, whitelist)
+                if mail_content is not None:
+                    whitelist_emails.append(mail_content)
 
-                    print('[Message]')
-                    if email_message.is_multipart():
-                        for part in email_message.get_payload():
-                            if part.get_content_type() == 'text/plain':
-                                bytes = part.get_payload(decode=True)
-                                encode = part.get_content_charset()
-                                message = message + str(bytes, encode)
-                    else:
-                        if email_message.get_content_type() == 'text/plain':
-                            bytes = email_message.get_payload(decode=True)
-                            encode = email_message.get_content_charset()
-                            message = str(bytes, encode)
-                    print(message)
+            return whitelist_emails
 
         except Exception as e:
             print("Fatal Error")
             traceback.print_exc() 
             print(str(e))
+            return None
 
-    def read_all_email_from_gmail(self):
-        try:
-            mail = imaplib.IMAP4_SSL(self.SMTP_SERVER)
-            mail.login(self.FROM_EMAIL, self.FROM_PWD)
-            mail.select('inbox')
-
-            data = mail.search(None, 'ALL')
-            mail_ids = data[1]
-            id_list = mail_ids[0].split()   
-            first_email_id = int(id_list[0])
-            latest_email_id = int(id_list[-1])
-
-            for i in range(latest_email_id,first_email_id, -1):
-                data = mail.fetch(str(i), '(RFC822)' )
-                for response_part in data:
-                    arr = response_part[0]
-                    if isinstance(arr, tuple):
-                        msg = email.message_from_string(str(arr[1],'utf-8'))
-                        email_subject = msg['subject']
-                        email_from = msg['from']
-                        print('From : ' + email_from + '\n')
-                        print('Subject : ' + email_subject + '\n')
-
-        except Exception as e:
-            print("Fatal Error")
-            traceback.print_exc() 
-            print(str(e))
 
 gmail = Gmail()
 
-gmail.read_latest_email()
+emails = gmail.read_latest_emails(['Facebook'])
 # gmail.read_all_email_from_gmail()
 
